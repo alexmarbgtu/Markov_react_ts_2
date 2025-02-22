3c41de8bacddb5f375e238b581f55acef9ed1161
Alex Markov <alex@mralex.su>
2025-02-22 16:04:35 +0300

    исправлены пути к шрифтам

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 import './index.css'
 import './components/theme-provider/themes.css'
 
-import './fonts/Roboto.ttf'
+import './assets/fonts/Roboto.ttf'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>
